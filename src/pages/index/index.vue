2dd7f5b370d51db37ead5bfaeed60df75bba88ed
bswh961229 <2356140334@qq.com>
2018-09-12 13:20:22 +0800

    第一次提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="header">
+      <icon type='search'></icon>
+      <input type="text" placeholder="搜索">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less">
+.header{
+  background-color: #ff2d4a;
+  padding:20rpx 16rpx;
+  position: relative;
+  icon{
+  position: absolute;
+  z-index: 998;
+  top:30rpx;
+  left:50%;
+  transform: translateX(-200%);
+  }
+  input{
+    text-align: center;
+    background-color: #ffffff;
+    height: 60rpx;
+    border-radius: 20rpx;
+    
+  }
+}
+</style>
+  
+
